2d74ab1ef11283a6920a01f698a8af60bea3a762
BrijeshSingh-dev <ssaa.brijesh@gmail.com>
2026-05-19 17:08:03 +0530

    19th May 2026-2

--- 2026/day-01/assignment.sh
+++ 2026/day-01/assignment.sh
@@ -1,3 +1,8 @@
+# Day 01 – Introduction to DevOps and Cloud
+
+## Task
+Today’s goal is to **set the foundation for your DevOps journey**.
+
 ## What is your understanding of DevOps and Cloud Engineering?
 # DevOps = Development + Operations
   DevOps is a practice that combines development and operations to automate software building, testing, deployment, and monitoring processes, ensuring faster and more reliable delivery. It focuses on CI/CD, automation, and collaboration.
