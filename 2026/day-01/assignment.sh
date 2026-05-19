38445aa0c565af4b0e37c146036baec9e404f5e6
BrijeshSingh-dev <ssaa.brijesh@gmail.com>
2026-05-19 17:04:45 +0530

    19th May 2026

--- 2026/day-01/assignment.sh
+++ 2026/day-01/assignment.sh
@@ -1,4 +1,11 @@
-# this is my first assigment
+## What is your understanding of DevOps and Cloud Engineering?
+# DevOps = Development + Operations
+  DevOps is a practice that combines development and operations to automate software building, testing, deployment, and monitoring processes, ensuring faster and more reliable delivery. It focuses on CI/CD, automation, and collaboration.
+
+  Cloud Engineering, on the other hand, involves designing, deploying, and managing scalable infrastructure on cloud platforms like AWS, Azure, or GCP. It includes resource provisioning, security, scalability, and cost optimization.
+
+  In simple terms, DevOps improves the software delivery lifecycle, while Cloud Engineering provides the infrastructure where applications run efficiently.
+
 
 
 # Assignment Day 1 done
